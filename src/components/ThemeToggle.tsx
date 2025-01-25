6e955e7f867abb4f4d30f1675cb336c71711aab2
Cihan TAYLAN <cihantaylan@cihantaylan.com>
2025-01-24 22:09:15 -0500

    Theme switch fixed

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   const isDark = resolvedTheme === "dark";
 
@@ -15,17 +25,15 @@ export default function ThemeToggle() {
     >
       <span className="sr-only">Toggle theme</span>
       <div
-        className={`${
-          isDark ? "translate-x-8 bg-transparent" : "translate-x-1 bg-white"
-        } inline-block h-5 w-5 transform rounded-full shadow-lg ring-0 transition duration-200 ease-in-out`}
+        className={`${isDark ? "translate-x-8 bg-transparent" : "translate-x-1 bg-white"
+          } inline-block h-5 w-5 transform rounded-full shadow-lg ring-0 transition duration-200 ease-in-out`}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className={`h-5 w-5 transition-colors duration-200 ${
-            isDark ? "text-white rotate-180" : "text-yellow-500 rotate-0"
-          }`}
+          className={`h-5 w-5 transition-colors duration-200 ${isDark ? "text-white rotate-180" : "text-yellow-500 rotate-0"
+            }`}
         >
           {isDark ? (
             <path
@@ -36,7 +44,7 @@ export default function ThemeToggle() {
           ) : (
             <path
               fillRule="evenodd"
-              d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
+              d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59z"
               clipRule="evenodd"
             />
           )}
